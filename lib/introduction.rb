d8a6a21020e088639a160609582736b08bde0709
Barak Rosner <rosnerbb@gmail.com>
2021-02-02 20:26:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction (name)
 end
 
 
-def introduction_with_language (name, language)
+def introduction_with_language (name = Barak, language = Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
